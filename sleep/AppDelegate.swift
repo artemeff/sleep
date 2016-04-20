d59d1b0508a2f430f860fd04490beb7f73f808ad
Yuri Artemev <i@artemeff.com>
2016-04-20 14:28:01 +0300

    Initial Commit

---
+++ sleep/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  sleep
+//
+//  Created by artemeff on 20/04/16.
+//  Copyright © 2016 artemeff. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
